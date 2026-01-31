6f40e888e07e53a99fc4c9170ead567b199c27f6
patriq128 <121152391+patriq128@users.noreply.github.com>
2026-01-31 22:12:12 +0100

    Add files via upload

---
+++ YoutSnake.py
@@ -0,0 +1,88 @@
+import yt_dlp
+import os
+import time
+
+def main():
+    os.system('printf "\\033[9;1t"')
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("\033[2J\033[H", end="")
+    print("""
+\033[91m█████ █████                      █████     \033[92m█████████                       █████              
+\033[91m▒▒███ ▒▒███                      ▒▒███     \033[92m███▒▒▒▒▒███                     ▒▒███               
+\033[91m ▒▒███ ███    ██████  █████ ████ ███████  \033[92m▒███    ▒▒▒  ████████    ██████   ▒███ █████  ██████ 
+\033[91m  ▒▒█████    ███▒▒███▒▒███ ▒███ ▒▒▒███▒   \033[92m▒▒█████████ ▒▒███▒▒███  ▒▒▒▒▒███  ▒███▒▒███  ███▒▒███
+\033[91m   ▒▒███    ▒███ ▒███ ▒███ ▒███   ▒███     \033[92m▒▒▒▒▒▒▒▒███ ▒███ ▒███   ███████  ▒██████▒  ▒███████ 
+\033[91m    ▒███    ▒███ ▒███ ▒███ ▒███   ▒███ ███ \033[92m███    ▒███ ▒███ ▒███  ███▒▒███  ▒███▒▒███ ▒███▒▒▒  
+\033[91m    █████   ▒▒██████  ▒▒████████  ▒▒█████ \033[92m▒▒█████████  ████ █████▒▒████████ ████ █████▒▒██████ 
+\033[91m   ▒▒▒▒▒     ▒▒▒▒▒▒    ▒▒▒▒▒▒▒▒    ▒▒▒▒▒   \033[92m▒▒▒▒▒▒▒▒▒  ▒▒▒▒ ▒▒▒▒▒  ▒▒▒▒▒▒▒▒ ▒▒▒▒ ▒▒▒▒▒  ▒▒▒▒▒▒                                                                                                                                                                                                
+                                                                                               """)
+    print("\033[0mAuthor: @patriq128")
+    print("\033[91m!!!For personal use only!!!")
+    print("""\033[0m*type "exit" for exit :3 """)
+    print("\033[0m----------------------------------------------------------------------------------------------------")
+    print("""\033[0m1.) Download \033[94mmp3""")
+    print("\033[0m----------------------------------------------------------------------------------------------------")
+    choose_main = input("\033[0mChoose option:")
+
+    if choose_main == "1":
+        mp3down()
+    elif choose_main == "exit":
+        exit()
+    else:
+        print("\033[91m!Wrong Input")
+        time.sleep(1)
+        main()
+
+def mp3down():
+
+    print("""\033[94m ██████   ██████ ███████████   ████████ 
+░░██████ ██████ ░░███░░░░░███ ███░░░░███
+ ░███░█████░███  ░███    ░███░░░    ░███
+ ░███░░███ ░███  ░██████████    ██████░ 
+ ░███ ░░░  ░███  ░███░░░░░░    ░░░░░░███
+ ░███      ░███  ░███         ███   ░███
+ █████     █████ █████       ░░████████ 
+░░░░░     ░░░░░ ░░░░░         ░░░░░░░░                                                                    
+                                        """)
+    print("""\033[92m                                                                
+▄█████ ▄▄  ▄▄  ▄▄▄  ▄▄ ▄▄ ▄▄▄▄▄ 
+▀▀▀▄▄▄ ███▄██ ██▀██ ██▄█▀ ██▄▄  
+█████▀ ██ ▀██ ██▀██ ██ ██ ██▄▄▄ 
+                                \033[0m""")
+    print("""*Paste link, click enter and you can paste another link and when you`re done just type "ok".""")
+    print("\033[0m----------------------------------------------------------------------------------------------------")
+    playlist_links = []
+
+    while True:
+        link = input("> ").strip()
+        if link.lower() == "ok":
+            break
+        if link:
+            playlist_links.append(link)
+        if link.lower() == "exit":
+            exit()
+
+    if not playlist_links:
+        print("exit")
+        exit()
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': 'Music/%(playlist_title)s/%(title)s.%(ext)s',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+        'ignoreerrors': True,
+        'quiet': False,
+    }
+
+    print("Start donwload")
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download(playlist_links)
+
+    print("done")
+
+main()
